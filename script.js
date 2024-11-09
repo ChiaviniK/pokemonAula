9bb5d2a05948b083c1b55a7d6757a66c6d7b7396
ChiaviniK <125836394+ChiaviniK@users.noreply.github.com>
2024-11-09 09:31:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showIncon(image){
+    const icon = image.nextElementSibling;
+    icon.style.display = 'block';
+}
+
+function hideIcon(image){
+    const icon = image.nextElementSibling;
+    icon.style.display = 'none';
+}
