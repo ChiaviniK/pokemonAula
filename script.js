1e8a03b29d8174887ebe1b08227c222bfdae015e
ChiaviniK <125836394+ChiaviniK@users.noreply.github.com>
2024-11-09 12:56:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function hideIcon(image){
     const icon = image.nextElementSibling;
     icon.style.display = 'none';
 }
+
+function setaa(){
+    document.getElementById('setaLuta').style.display = 'block';
+}
+function setae(){
+    document.getElementById('setaLuta').style.display = 'none';
+}
