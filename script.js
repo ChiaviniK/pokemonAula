7ad9320249cfef6734eb9482b2bf7506f9d6c0ab
ChiaviniK <125836394+ChiaviniK@users.noreply.github.com>
2024-11-16 13:00:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let vida1 = 166;
+let vida2 = 200;
+vidaCompletaOponente = (vida1*100)/90;
+
 function showIncon(image){
     const icon = image.nextElementSibling;
     icon.style.display = 'block';
@@ -14,3 +18,45 @@ function setaa(){
 function setae(){
     document.getElementById('setaLuta').style.display = 'none';
 }
+
+function ataque(){
+    document.getElementById('ataqueJogador').style.display = 'block';
+}
+
+function esconderAtaque(){
+    document.getElementById('ataqueJogador').style.display = 'none';
+}
+
+function ataque1(){
+    esconderAtaque();
+    novaVidaOponente = vida1 - 20;
+    vida1 = novaVidaOponente;
+    novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+    
+    document.getElementById('vidaPkmOponente').style.width = `${novaVidaP}%`;
+}
+function ataque2(){
+    esconderAtaque();
+    novaVidaOponente = vida1 - 20;
+    vida1 = novaVidaOponente;
+    novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+    
+    document.getElementById('vidaPkmOponente').style.width = `${novaVidaP}%`;
+}
+function ataque3(){
+    esconderAtaque();
+    novaVidaOponente = vida1 - 20;
+    vida1 = novaVidaOponente;
+    novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+    
+    document.getElementById('vidaPkmOponente').style.width = `${novaVidaP}%`;
+}
+function ataque4(){
+    esconderAtaque();
+    novaVidaOponente = vida1 - 20;
+    vida1 = novaVidaOponente;
+    novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+    
+    document.getElementById('vidaPkmOponente').style.width = `${novaVidaP}%`;
+}
+
